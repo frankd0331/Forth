5eeef0dce28de0df5ef61bd6d5f16a23c06ed93c
Frank Dudek <dudek.f.p@gmail.com>
2023-04-13 21:16:07 +0700

    gonna try to change stuff

--- Program.fs
+++ Program.fs
@@ -10,8 +10,37 @@ let push n l = n::l
 
 let pop l =
     match l with
-    | [] -> failwith "can't pop an empty stack"
-    | x::xs -> x, xs
+    | [] -> printfn "Error: can't pop an empty stack"
+            None
+    | x::xs -> Some(x, xs)
+    
+let peek l =
+    match l with
+    | [] -> printfn "Error: can't peek on an empty stack"
+            None
+    | x::_ -> Some(x,l)
+    
+let duplicate (result: (int * int list) option) =
+    match result with
+    | None -> None
+    | Some (n, stack) -> Some(n, n::stack)
+    
+let unaryOperation (result: (int * int list) option) (operation: int->int) =
+    match result with
+    | None -> None
+    | Some (n, stack) ->
+        let r = (operation n)
+        Some (r, r::stack)
+    
+let binaryOperation (result: (int * int list) option) (operation: int->int->int) =
+    match result with
+    | None -> None
+    | Some (second, stack) ->
+        match pop stack with
+        | None -> None
+        | Some (first, stack) ->
+            let n = (operation first second)
+            Some (n, n::stack)
 
 let tokenize input =
     let regex = Regex("(\\s*[()\\[\\]{}'\`~,^@\\s]\\s*|;.*|[^\\s,()\\[\\]{}\"'`~@^;]+)")
@@ -29,7 +58,8 @@ let matchWord word =
     | "+" | "-" | "/" | "*" | "%"
     | "."
     | "add" | "sub" | "div" | "mul" | "mod"
-    | "negate" | "abs" | "max" | "min" -> Operator word
+    | "negate" | "abs" | "max" | "min"
+    | "dup" | "swap" | "rot" | "drop" | "nip" -> Operator word
     | ".s" -> PrintCommand
     | _ -> Number (int word) 
 
@@ -41,7 +71,7 @@ let rec readTokens tokens =
 let read input =
     input |> tokenize |> readTokens
 
-let rec eval (words:Word list, stack:int list) =
+let rec eval words stack =
     match words with
     | [] -> printfn "ok"
             stack
@@ -50,35 +80,29 @@ let rec eval (words:Word list, stack:int list) =
         | PrintCommand ->
                           let stackStr = stack |> List.rev |> List.map string |> String.concat " " 
                           printf $"<{List.length(stack)}> {stackStr} "
-                          eval(xs, stack)
-        | Number n -> eval(xs, push n stack)
+                          eval xs stack
+        | Number n -> eval xs (push n stack)
         | Operator op ->
-            match op with
-            | "+" | "add" ->
-                let second, stack = pop stack
-                let first, stack = pop stack
-                eval(xs, push (first + second) stack)
-            | "-" | "sub" ->
-                let second, stack = pop stack
-                let first, stack = pop stack
-                eval(xs, push (first-second) stack)
-            | "*" | "mul" ->
-                let second, stack = pop stack
-                let first, stack = pop stack
-                eval(xs, push (first*second) stack)
-            | "/" | "div" ->
-                let second, stack = pop stack
-                let first, stack = pop stack
-                eval(xs, push (first/second) stack)
-            | "%" | "mod" ->
-                let second, stack = pop stack
-                let first, stack = pop stack
-                eval(xs, push (first%second) stack)
-            | "." ->
-                let p, stack = pop stack
-                printf $"{p} "
-                eval(xs, stack)
-            | _ -> failwith "not implemented"
+            let newStack = 
+                match op with
+                | "+" | "add" -> binaryOperation (pop stack) (+)
+                | "-" | "sub" -> binaryOperation (pop stack) (-)
+                | "*" | "mul" -> binaryOperation (pop stack) (*)
+                | "/" | "div" -> binaryOperation (pop stack) (/)
+                | "%" | "mod" -> binaryOperation (pop stack) (%)
+                | "min" -> binaryOperation (pop stack) (fun a b -> if a < b then a else b)
+                | "max" -> binaryOperation (pop stack) (fun a b -> if a > b then a else b)
+                | "negate" -> unaryOperation (pop stack) (fun n -> n * -1)
+                | "abs" -> unaryOperation (pop stack) (fun n -> if n < 0 then -n else n)
+                | "dup" -> duplicate (peek stack)
+                | "." ->
+                    match pop stack with
+                    | None -> None
+                    | Some (p, stack) -> printf $"{p} "; Some (p, stack)
+                | _ -> failwith "not implemented"
+            match newStack with
+            | None -> eval xs stack
+            | Some (_, stack) -> eval xs stack
                      
         
     
@@ -95,6 +119,6 @@ let main argv =
         | "bye" -> ()
         | _ ->
                let words = input |> read
-               loop(eval (words, stack))
+               loop(eval words stack)
     loop(List.Empty)
     0 // return an integer exit code
